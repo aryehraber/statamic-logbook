5eb55f73106be46f946dacbbe7bbedba6519d207
Aryeh Raber <aryeh.raber@gmail.com>
2023-05-10 11:26:20 +0200

    Update LogbookServiceProvider.php

--- src/LogbookServiceProvider.php
+++ src/LogbookServiceProvider.php
@@ -2,30 +2,29 @@
 
 namespace AryehRaber\Logbook;
 
-use Statamic\Facades\Utility;
 use Illuminate\Routing\Router;
+use Statamic\Facades\Utility;
 use Statamic\Providers\AddonServiceProvider;
 
 class LogbookServiceProvider extends AddonServiceProvider
 {
+    protected $viewNamespace = 'logbook';
+
     protected $scripts = [
         __DIR__.'/../resources/js/logbook.js'
     ];
 
-    public function boot()
+    public function bootAddon()
     {
-        parent::boot();
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'logbook');
-
-        Utility::make('logbook')
-            ->title(__('Logbook'))
-            ->icon('book-pages')
-            ->description(__('Manage and view log files.'))
-            ->routes(function (Router $router) {
-                $router->get('/', [LogbookController::class, 'show'])->name('show');
-                $router->delete('/delete', [LogbookController::class, 'destroy'])->name('destroy');
-            })
-            ->register();
+        Utility::extend(function () {
+            Utility::register('logbook')
+                ->title(__('Logbook'))
+                ->icon('book-pages')
+                ->description(__('Manage and view log files.'))
+                ->routes(function (Router $router) {
+                    $router->get('/', [LogbookController::class, 'show'])->name('show');
+                    $router->delete('/delete', [LogbookController::class, 'destroy'])->name('destroy');
+                });
+        });
     }
 }
